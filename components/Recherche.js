23c2f21f88b9498441c84655e39fc5ab36299e79
laure <laurent.cocheteux@hotmail.fr>
2022-03-05 12:16:35 +0100

    news

--- components/Recherche.js
+++ components/Recherche.js
@@ -1,8 +1,118 @@
-import React, { useState } from "react";
-import { StyleSheet, View, TextInput, Pressable, Text } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, View, TextInput, Pressable, Text, Image } from 'react-native';
+import Voice from 'react-native-voice';
 
 const Form = ({ navigation }) => {
+    const [pitch, setPitch] = useState('');
+    const [error, setError] = useState('');
+    const [end, setEnd] = useState('');
+    const [started, setStarted] = useState('');
+    const [results, setResults] = useState([]);
+    const [partialResults, setPartialResults] = useState([]);
     const [myValueFilm, setValueFilm] = useState('');
+
+
+    useEffect(() => {
+        Voice.onSpeechStart = onSpeechStart;
+        Voice.onSpeechEnd = onSpeechEnd;
+        Voice.onSpeechError = onSpeechError;
+        Voice.onSpeechResults = onSpeechResults;
+        Voice.onSpeechPartialResults = onSpeechPartialResults;
+        Voice.onSpeechVolumeChanged = onSpeechVolumeChanged;
+        return () => {
+            //destroy the process after switching the screen
+            Voice.destroy().then(Voice.removeAllListeners);
+        };
+    }, [])
+
+    const onSpeechStart = (e) => {
+        //Invoked when .start() is called without error
+        console.log('onSpeechStart: ', e);
+        setStarted('√');
+    };
+
+    const onSpeechEnd = (e) => {
+        //Invoked when SpeechRecognizer stops recognition
+        console.log('onSpeechEnd: ', e);
+        setEnd('√');
+    };
+
+    const onSpeechError = (e) => {
+        //Invoked when an error occurs.
+        console.log('onSpeechError: ', e);
+        setError(JSON.stringify(e.error));
+    };
+
+    const onSpeechResults = (e) => {
+        //Invoked when SpeechRecognizer is finished recognizing
+        console.log('onSpeechResults: ', e);
+        setResults(e.value);
+    };
+
+    const onSpeechPartialResults = (e) => {
+        //Invoked when any results are computed
+        console.log('onSpeechPartialResults: ', e);
+        setPartialResults(e.value);
+    };
+
+    const onSpeechVolumeChanged = (e) => {
+        //Invoked when pitch that is recognized changed
+        console.log('onSpeechVolumeChanged: ', e);
+        setPitch(e.value);
+    };
+    const startRecognizing = async () => {
+        //Starts listening for speech for a specific locale
+        try {
+            await Voice.start('en-US');
+            setPitch('');
+            setError('');
+            setStarted('');
+            setResults([]);
+            setPartialResults([]);
+            setEnd('');
+        } catch (e) {
+            //eslint-disable-next-line
+            console.error(e);
+        }
+    };
+
+    const stopRecognizing = async () => {
+        //Stops listening for speech
+        try {
+            await Voice.stop();
+        } catch (e) {
+            //eslint-disable-next-line
+            console.error(e);
+        }
+    };
+
+    const cancelRecognizing = async () => {
+        //Cancels the speech recognition
+        try {
+            await Voice.cancel();
+        } catch (e) {
+            //eslint-disable-next-line
+            console.error(e);
+        }
+    };
+
+    const destroyRecognizer = async () => {
+        //Destroys the current SpeechRecognizer instance
+        try {
+            await Voice.destroy();
+            setPitch('');
+            setError('');
+            setStarted('');
+            setResults([]);
+            setPartialResults([]);
+            setEnd('');
+        } catch (e) {
+            //eslint-disable-next-line
+            console.error(e);
+        }
+    };
+
+
     return (
 
         <View style={styles.Container}>
@@ -13,6 +123,8 @@ const Form = ({ navigation }) => {
                 value={myValueFilm}
 
             />
+
+
 
             <Pressable onPress={() => {
                 navigation.navigate('ListFilm', {
@@ -27,7 +139,9 @@ const Form = ({ navigation }) => {
 
                 </View>
             </Pressable>
+            <Image style={{ width: 50, height: 50 }} source={require('../assets/microphone-forme-noire.png')}></Image>
         </View >
+
     );
 }
 //style={{ borderRadius: 15, backgroundColor: '#6CBAFC', padding: 5, top: 50 }}
